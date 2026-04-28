680a5bd9c2c80cf9ed84a5a2413379dfefcd2e6b
roizherrerapilar-hub <roizherrerapilar@gmail.com>
2026-04-28 13:03:08 +0200

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,48 @@
+SHOW DATABASES;
+
+USE sakila;
+SHOW TABLES;
+
+SELECT * FROM actor;
+SELECT * FROM film;
+SELECT * FROM customer;
+
+SELECT title FROM film;
+SELECT name AS language FROM language; 
+SELECT first_name FROM staff; 
+
+SELECT DISTINCT release_year FROM film;
+
+SELECT COUNT(*) FROM store;
+SELECT COUNT(*) FROM staff;
+SELECT COUNT(*) FROM film;
+SELECT COUNT(*) FROM rental;
+SELECT COUNT(DISTINCT last_name) FROM actor;
+
+SELECT title, length
+FROM film
+ORDER BY length DESC
+LIMIT 10;
+
+SELECT * 
+FROM actor
+WHERE first_name = 'SCARLETT';
+
+SELECT title, length
+FROM film
+WHERE title LIKE '%ARMAGEDDON%'
+AND length > 100;
+
+SELECT COUNT(*)
+FROM film
+WHERE special_features LIKE '%Behind the Scenes%';
+
+
+
+
+
+
+
+
+
+
